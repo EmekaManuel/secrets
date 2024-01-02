986f624938c8ede461850d9e62eecefdf9300e11
Onyewuchi Emeka <marrnuel123@.com>
2024-01-02 20:55:00 +0100

    chore(auth): setup nextauth provider

--- app/api/auth/[...nextAuth]/route.ts
+++ app/api/auth/[...nextAuth]/route.ts
@@ -1,3 +1,49 @@
-import nextAuth from 'next-auth';
-import type { AuthOptions } from 'next-auth';
-import { CredentialsProvider } from 'next-auth/providers/credentials';
+import NextAuth, { AuthOptions } from 'next-auth';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import bcrypt, { compare } from 'bcryptjs';
+
+import prismadb from '@/lib/prismadb';
+import CredentialsProvider from 'next-auth/providers/credentials';
+
+export const authOptions: AuthOptions = {
+ adapter: PrismaAdapter(prismadb),
+ providers: [
+  CredentialsProvider({
+   name: 'Credentials',
+   credentials: {
+    email: { label: 'Email', type: 'text', placeholder: 'Email' },
+    password: { label: 'Password', type: 'password' },
+   },
+   async authorize(credentials, req) {
+    if (!credentials?.email || !credentials?.password) {
+     throw new Error('Email and Password Required');
+    }
+
+    const user = await prismadb.user.findUnique({
+     where: { email: credentials.email },
+    });
+
+    if (!user || !user.hashedPassword) {
+     throw new Error("Email doesn't exist");
+    }
+    const isCorrectPassword = await compare(
+     credentials.password,
+     user.hashedPassword,
+    );
+
+    if (!isCorrectPassword) {
+     throw new Error('Incorrect Password');
+    }
+
+    return user;
+   },
+  }),
+ ],
+ session: {
+  strategy: 'jwt',
+ },
+ secret: process.env.NEXTAUTH_SECRET,
+ debug: process.env.NODE_ENV === 'development',
+};
+
+export default NextAuth(authOptions);
